71d8b14798bcb70c8b4aafa1363dab2b95a1a470
sy2681 <yn3819@naver.com>
2023-12-25 23:03:24 +0900

    ng 설치: ng new test

---
+++ test/a.module.ts
@@ -0,0 +1,5 @@
+export class TeComponent {
+
+}
+
+// 하나의 파일=하나의 모듈
---
+++ test/b.module.ts
@@ -0,0 +1,3 @@
+import { TeComponent } from "./a.module";
+  // import * as LayoutModule from './a.module' js로 치면 이거나 마찬가지
+import * as LayoutModule from './a.module'
